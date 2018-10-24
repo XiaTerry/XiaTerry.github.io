5af10c779218661f7be31be07f66e50b11d513c5
XiaTerry <1363314380@qq.com>
2018-10-24 22:26:27 +0800

    Site updated: 2018-10-24 22:26:26

--- about/js/index.js
+++
@@ -1,7 +0,0 @@
-var oHtml=document.getElementById('html');
-setInterval(function () {
-    if(oHtml.offsetWidth>=oHtml.parentNode.offsetWidth){
-        oHtml.style.width = 0;
-    }
-    oHtml.style.width=oHtml.offsetWidth + 10 +'px';
-},100);
